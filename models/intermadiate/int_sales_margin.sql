7b0d10e83c73c50e916a8a44596da524176098b9
Orlando Zeng <orlando@global-re.com>
2025-02-18 14:12:27 +0000

    error date

--- models/intermadiate/int_sales_margin.sql
+++ models/intermadiate/int_sales_margin.sql
@@ -8,13 +8,13 @@ product as (
     select * from {{ ref('stg_product') }}
 )
 
-select 
-    sales.date_date,
+select
     sales.orders_id,
     sales.pdt_id,
     sales.quantity,
     sales.revenue,
     product.purchase_price,
+    cast(sales.date_date as DATE) as date_date,
     cast(sales.quantity * product.purchase_price as FLOAT64) as purchase_cost,
     cast(sales.revenue - (sales.quantity * product.purchase_price) as FLOAT64) as margin
 from sales
