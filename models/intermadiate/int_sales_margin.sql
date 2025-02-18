323e1d149ff74d044fefd1b9dbb16e50c5ea780d
Orlando Zeng <orlando@global-re.com>
2025-02-18 13:12:02 +0000

    Sales Margin

---
+++ models/intermadiate/int_sales_margin.sql
@@ -0,0 +1,21 @@
+with 
+
+sales as (
+    select * from {{ ref('stg_sales') }}
+),
+
+product as (
+    select * from {{ ref('stg_product') }}
+)
+
+select 
+    sales.orders_id,
+    sales.pdt_id,
+    sales.quantity,
+    sales.revenue,
+    product.purchase_price,
+    cast(sales.quantity * product.purchase_price as FLOAT64) as purchase_cost,
+    cast(sales.revenue - (sales.quantity * product.purchase_price) as FLOAT64) as margin
+from sales
+left join product
+on sales.pdt_id = product.products_id
